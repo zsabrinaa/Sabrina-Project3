b63855db3cbfd5d3990695d77ddfbaa528db29db
Sabrina <zsbrina20@gmail.com>
2019-05-13 21:27:29 -0500

    .

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -11,7 +11,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Item
+    db.Item   
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
