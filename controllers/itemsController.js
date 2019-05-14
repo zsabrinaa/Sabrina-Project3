f60b67ffcf79c5332f9d1e9209657e9d9bb16343
Sabrina <zsbrina20@gmail.com>
2019-05-13 21:46:27 -0500

    change file name

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,5 +1,5 @@
-const db = require("../models");
-// Defining methods for the booksControllers
+// const db = require("../models");
+
 module.exports = {
   findAll: function(req, res) {
     console.log(req.query)
